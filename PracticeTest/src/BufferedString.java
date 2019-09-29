c5b89e8c638eb4869ad7d67356549c2db48f8c0c
Narasimha <Narasimha@Narasimha-PC>
2019-09-29 18:44:55 +0530

    StringBuffer program executed and pushed to Git successfully.

---
+++ PracticeTest/src/BufferedString.java
@@ -0,0 +1,15 @@
+
+public class BufferedString {
+	
+	public static void methodrev(String str)
+		{	
+			StringBuffer bfs = new StringBuffer(str);
+			 System.out.println(bfs.reverse());
+		}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		methodrev("Madugam");
+	}
+
+}
