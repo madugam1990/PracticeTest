cd730fd00563f25e122b8e2549c2b9f1d80f8b59
Narasimha <Narasimha@Narasimha-PC>
2019-09-29 19:16:48 +0530

    comiting sum of digits to branchX

---
+++ PracticeTest/src/SumOfDigits.java
@@ -0,0 +1,14 @@
+
+public class SumOfDigits {
+	
+	public static void methodSum(int x, int y)
+		{System.out.println(x+y);
+		
+		}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SumOfDigits.methodSum(19,14);
+	}
+
+}
